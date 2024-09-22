4ad57f6e8019409f6c0a02799cedb9ac6fe24466
ngokay <61977474+ngokay@users.noreply.github.com>
2024-09-22 22:30:28 +0700

    commit

--- WebApplication5/Controllers/WeatherForecastController.cs
+++ WebApplication5/Controllers/WeatherForecastController.cs
@@ -12,7 +12,7 @@ namespace WebApplication5.Controllers
 
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","anhnt93"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","anhnt94"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
